cbb9cc1563d8a1b0c7c932b3da08e8e4c6a35c4a
Pavel Diatchenko <diatche@gmail.com>
2015-07-09 20:17:24 +1000

    Initial Commit

---
+++ PDSharingCenter/PDSharingCenter.h
@@ -0,0 +1,19 @@
+//
+//  PDSharingCenter.h
+//  PDSharingCenter
+//
+//  Created by Pavel Diatchenko on 9/07/2015.
+//  Copyright (c) 2015 Pavel Diatchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PDSharingCenter.
+FOUNDATION_EXPORT double PDSharingCenterVersionNumber;
+
+//! Project version string for PDSharingCenter.
+FOUNDATION_EXPORT const unsigned char PDSharingCenterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PDSharingCenter/PublicHeader.h>
+
+
